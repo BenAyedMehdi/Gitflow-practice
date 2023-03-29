17bcab4770141b923981eed2e72c62ff2a11a400
Ben Ayed Mehdi <mehdibenayed97@gmail.com>
2023-03-29 02:12:26 +0200

    create project and add interface

---
+++ Calculator/Interfaces/IEquation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Interfaces
+{
+    public interface IEquation
+    {
+        public double Perform(double num1, double num2);
+    }
+}
